88c554820f5d4fb77116d38121a5111524c31d3d
FranciscoEspinozaG <francisco.espinoza@oqo.cl>
2019-11-21 12:13:16 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+	------------------------------------------------------------------
+		Buscador en selectores
+	------------------------------------------------------------------
+	*/
+
+	jQuery(document).ready(function($){
+		$(document).ready(function(){
+			$('#region-comuna').select2();
+		});
+	});
